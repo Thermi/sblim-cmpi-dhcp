201531b44ed1202607dc22056c550595b4a6d86c
Jeremy Bongio <jbongio@us.ibm.com>
2009-09-16 02:34:27 -0400

    Changed license from CPL to EPL.

--- provider/Linux_DHCPElementConformsToProfile/Linux_ElementConformsToProfile_Resource.h
+++ provider/Linux_DHCPElementConformsToProfile/Linux_ElementConformsToProfile_Resource.h
@@ -1,12 +1,12 @@
 // ============================================================================
 // Copyright © 2007, International Business Machines
 //
-// THIS FILE IS PROVIDED UNDER THE TERMS OF THE COMMON PUBLIC LICENSE
+// THIS FILE IS PROVIDED UNDER THE TERMS OF THE ECLIPSE PUBLIC LICENSE
 // ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS FILE
 // CONSTITUTES RECIPIENTS ACCEPTANCE OF THE AGREEMENT.
 //
-// You can obtain a current copy of the Common Public License from
-// http://oss.software.ibm.com/developerworks/opensource/license-cpl.html
+// You can obtain a current copy of the Eclipse Public License from
+// http://www.opensource.org/licenses/eclipse-1.0.php
 //
 // Authors:             Sam Kannan <samkannan (at) us.ibm.com>
 // ============================================================================
